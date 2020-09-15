fa2e81f9ff58801edbfaea133a2b26f1a241223c
masonauseth <826102@mystma.org>
2020-09-15 20:15:52 +0000

    variables

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,19 +2,15 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Mason");
-        System.out.println("code is hard");
-        System.out.println("cool beans");
-        System.out.println("Ha Ha Code go click click");
-        System.out.println("The goonies");
-        System.out.println("The gremlins");
-        System.out.println("The matrix");
-        System.out.println("My best friend wedding");
-        System.out.println("Finding Nemo");
-        System.out.println("Inseption");
-        System.out.println("Endgame");
-        System.out.println("sighns");
-        System.out.println("the 6th sence");
-        System.out.println("beetle Juice");
+        boolean amICool = true; 
+        System.out.println(amICool);
+        byte numOfCarsIHave = 2;
+        System.out.println(numOfCarsIHave);
+        char fisrtLetterOfLion = 'L';
+        System.out.println(fisrtLetterOfLion);
+        short NumOfWallsInSchoole = 400;
+        System.out.println(NumOfWallsInSchoole);
+        
+
     }
 }
