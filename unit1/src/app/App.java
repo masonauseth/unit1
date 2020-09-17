d7667080cb673a25469354ead68be5dbf31d9145
masonauseth <826102@mystma.org>
2020-09-17 18:10:51 +0000

    madlib

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,23 +2,34 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean amICool = true; 
-        System.out.println(amICool);
-        byte numOfCarsIHave = 2;
-        System.out.println(numOfCarsIHave);
-        char fisrtLetterOfLion = 'L';
-        System.out.println(fisrtLetterOfLion);
-        short numOfWallsInSchoole = 400;
-        System.out.println(numOfWallsInSchoole);
-        int numOfTwinsInWorld = 40000000;
-        System.out.println(numOfTwinsInWorld);
-        long numOfStarsInSky = 54775807;
-        System.out.println(numOfStarsInSky);
-        float chanceOfMeTurnigIntoDuck = 6;
-        System.out.println(chanceOfMeTurnigIntoDuck);
-        double chanceOfPhoneCracking = 25;
-        System.out.println(chanceOfPhoneCracking);
+
+        String person1 = "Tom";
+        String verb1 = "dance";
+        System.out.println(person1 + " is a programmer who likes to " + verb1 + ".");
+        String dayOfWeek = "Monday";
+        String adjective1 = "stale";
+        String food = "grape";
+        String verb2 = "jumps";
+        String place1 = "a bridge";
+        String person2 = "Herald";
+        System.out.println("Every " + dayOfWeek + " he makes a " + adjective1 + " " + food + " for breakfast and " + verb2 + " at " + place1 + " with " + person2 + ".");
+        String adjective2 = "old";
+        String place2 = "warehouse";
+        String verb3 = "chill";
+        System.out.println("Sometimes, he goes to the " + adjective2 + " " + place2 + " with " + person2 + " so that they can " + verb3 + " together.");
+
+
+
+
+    
+      
+    
         
+
+
+
+
+
 
 
         
