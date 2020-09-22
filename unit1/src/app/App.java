633e6b31f7f9520786e0cde4cb74f4a77f3059d3
masonauseth <826102@mystma.org>
2020-09-22 22:56:02 +0000

    blueblueblue

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -7,19 +7,19 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Lets do a MadLib!");
-        System.out.println("Enter any name.");
-        String name1 = input.nextLine();
-        System.out.println("Enter any food.");
-        String food = input.nextLine();
-        System.out.println("Enter another name.");
-        String name2 = input.nextLine();
-        System.out.println("Enter a verb.");
-        String verb = input.nextLine();
-        System.out.println("Enter any city.");
-        String place = input.nextLine();
-        System.out.println(name1 + " loves to make " + food + "s with " + name2 + ".");
-        System.out.println("After that they both " + verb + " all the way to " + place + ".");
+
+
+        System.out.println("Enter a color");
+        String color = input.nextLine();
+        System.out.println("Enter a number");
+        int number = input.nextInt();
+
+        while (true) {
+            System.out.println(color*number);
+        }
+
+
+
 
 
 
