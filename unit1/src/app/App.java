04c48a5c9aeeda8424ecf52b9f7edaa461081a5c
masonauseth <826102@mystma.org>
2020-09-25 16:59:31 +0000

    lotsofnumbers

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -8,19 +8,38 @@ public class App {
         Scanner input = new Scanner(System.in);
 
 
+        for(int i = 0; i <= 10; i = i + 1){
+            System.out.println("the count is " + i);
+            System.out.println("Lets keep counting");
 
-        System.out.println("Enter a color");
-        String color = input.nextLine();
-
-        System.out.println("Enter a number");
-        int number = input.nextInt();
-
-        int count = 0;
-
-        while(count < number) {
-            System.out.println(color);
-            count = count +1;
         }
+        for(int k = 10; k <= 20; k = k + 1){
+            System.out.println("the count is " + k);
+            System.out.println("go go gooooo");
+        }
+        for(int j = 10; j <= 100; j = j + 1){
+            System.out.println("the count is " + j);
+            System.out.println("higher higher higher");
+        }
+        for(int t = 1000; t <= 256000; t = t + 1){
+            System.out.println("the count is " + t);
+            System.out.println("clime clime clime");
+        }
+        for(int v = 11; v <= 12; v = v + 1){
+            System.out.println("the count is " + v);
+            System.out.println("woah woah woah slow down!");
+        }
+        for(int n = 10; n >= 0; n = n - 1){
+            System.out.println("the count is " + n);
+            System.out.println("noooo, I said slow down not go backwards!!!");
+        }
+        System.out.println("ok, phew, it stoped");
+
+        
+
+
+    
+
 
 
 
