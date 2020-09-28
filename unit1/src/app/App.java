1cb52b60818e5c07ab8731ac0a744e3efd49c526
masonauseth <826102@mystma.org>
2020-09-28 20:14:35 +0000

    questions

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,72 +1,34 @@
 package app;
-import java.util.Scanner;
 
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner input = new Scanner(System.in);
 
+        System.out.println("What is my name?");
 
-        for(int i = 0; i <= 10; i = i + 1){
-            System.out.println("the count is " + i);
-            System.out.println("Lets keep counting");
-
+        String answer1 = input.nextLine();
+        if (answer1.equals("Mason")){
+            System.out.println("correct");
         }
-        for(int k = 10; k <= 20; k = k + 1){
-            System.out.println("the count is " + k);
-            System.out.println("go go gooooo");
+        else {
+            System.out.println("Wrong");
         }
-        for(int j = 10; j <= 100; j = j + 1){
-            System.out.println("the count is " + j);
-            System.out.println("higher higher higher");
+
+        System.out.println("Who is the President?");
+
+        String answer2 = input.nextLine();
+        if (answer2.equals("Donald Trump")){
+            System.out.println("correct");
         }
-        for(int t = 1000; t <= 256000; t = t + 1){
-            System.out.println("the count is " + t);
-            System.out.println("clime clime clime");
+        else{
+            System.out.println("wrong");
         }
-        for(int v = 11; v <= 12; v = v + 1){
-            System.out.println("the count is " + v);
-            System.out.println("woah woah woah slow down!");
-        }
-        for(int n = 10; n >= 0; n = n - 1){
-            System.out.println("the count is " + n);
-            System.out.println("noooo, I said slow down not go backwards!!!");
-        }
-        System.out.println("ok, phew, it stoped");
-
-        
-
-
-    
 
 
 
-
-
-
-
-
-        
-        
-
-    
-
-
-
-
-    
-      
-    
-        
-
-
-
-
-
-
-
-        
 
     }
 }
