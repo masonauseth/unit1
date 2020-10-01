4f4b510aa35128f611ab0327950906e71545cc19
masonauseth <826102@mystma.org>
2020-10-01 15:29:52 +0000

    ihatethis

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -7,63 +7,20 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        int correct1 = 0;
+        System.out.println("Lets learn about elephants. There are 5 facts, simply type a number from 1/5 and receive a fact.");
 
-        System.out.println("Lets take a short test, dont use capitals unless its a name.");
+        String fact1 = "elephants are mammlals";
 
-        System.out.println("What is my creators name?");
+        String fact2 = "elephants are the largest land animal";
 
-        String answer1 = input.nextLine();
-        if (answer1.equals("Mason")){
-            System.out.println("correct");
-            correct1 = correct1 + 1;
-        }
-        else {
-            System.out.println("Wrong");
-        }
+        int fact1n = 1;
 
-        System.out.println("Who is the President?");
+        
 
-        String answer2 = input.nextLine();
-        if (answer2.equals("Donald Trump")){
-            System.out.println("correct");
-            correct1 = correct1 + 1;
-        }
-        else{
-            System.out.println("wrong");
-        }
-        System.out.println("Where does paper come from?");
+        
 
-        String answer3 = input.nextLine();
-        if (answer3.equals("trees")){
-            System.out.println("correct");
-            correct1 = correct1 +1;
-        }
-        else {
-            System.out.println("wrong");
-        }
-        System.out.println("What do you see with?");
-        String answer4 = input.nextLine();
-        if (answer4.equals("eyes")){
-            System.out.println("correct");
-            correct1 = correct1 + 1;
-        }
-        else {
-            System.out.println("wrong");
-        }
-        System.out.println("What is the first meal of the day?");
-        String answer5 = input.nextLine();
-        if (answer5.equals("breakfast")){
-            System.out.println("correct");
-            correct1 = correct1 + 1;
-        }
-        else {
-            System.out.println("wrong");
-        }
 
-        int per = correct1 * 20;
-
-        System.out.println("Your score is " + correct1 + "/5 " + "or " + per + "%");
+       
 
 
 
