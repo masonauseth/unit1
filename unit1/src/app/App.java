3dadfc5451dc2562426bbe9760173b0071850869
masonauseth <826102@mystma.org>
2020-10-20 20:14:22 +0000

    vagasbaby

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
@@ -7,21 +8,50 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
+        Random ran = new Random();
+
+        int nl = ran.nextInt(36);
+
+        int money = 100;
+
        System.out.println("We will be playing the Roulette wheel!");
 
        System.out.println("You have $100");
 
-       System.out.println("Would you like to make a inside or outside bet");
+       System.out.println("Would you like to make a inside or outside bet?");
 
       String bettype = input.nextLine();
 
       if(bettype.equals("inside")){
-          System.out.println("what number do you want to bet on");
-          
+          System.out.println("What number from 0 to 36 do you want to bet on?");
+          int insidebet = input.nextInt();
+          System.out.println("How much do you want to bet?");
+          int amountbet = input.nextInt();
+          System.out.println("You bet " + insidebet);
+          System.out.println("Spining...");
+          System.out.println("The number is " + nl);
+          if(insidebet == nl){
+              System.out.println("You Won!");
+              System.out.println("Your money is now " + (money + amountbet * 35));
+          }
+          else{
+              System.out.println("You Lost :(");
+              System.out.println("Your money is now " + (money - amountbet));
+          }
       }
-      else{
+      else if(bettype.equals("outside")){
+          System.out.println("Would you like to bet on red, black, even, or odd?");
+          String outsidebet = input.nextLine();
+          System.out.println("You bet " + outsidebet);
+      }
 
-      }
+      
+
+
+
+     
+      
+      
 
         
     }
